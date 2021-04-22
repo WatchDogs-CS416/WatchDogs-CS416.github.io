f1efee73a6b3082e037d7bb4ea6d9bae35b67f5f
AmeyGohil <amey23399@gmail.com>
2021-04-22 17:20:28 +0530

    All Pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
-function foo() {
-	console.log("Team Watch Dogs | CS 416[2020]");
-}
 
+hljs.highlightAll();
+
+function foo() {
+	console.log("Team Watch Dogs | CS 416[2021] | IIT Bombay");
+}
 (function ($)
 { "use strict"
-
+	$(".side-menu-content>ul>li>a").on("click",function (e){
+		e.preventDefault();
+		let target = $(this).attr('href');
+		if($(window).width()<=786){
+			$(".slided").removeClass('slided');
+			$(".hide").removeClass('hide');
+			$("#close_btn").toggleClass('shw');
+			$('html, body').animate({
+				scrollTop: $(target).offset().top - 56 // Means Less header height
+			},10);
+		}
+		else{
+			$('html, body').animate({
+				scrollTop: $(target).offset().top - 56 // Means Less header height
+			},10);
+		}
+	})
 })(jQuery);
